99e69736dbfccdc2f721b5997b8982fb09c76052
dmagnant <danielmagnant@northwesternmutual.com>
2022-01-21 17:23:25 -0600

    commit

--- src/Components/PortfolioWorth.js
+++ src/Components/PortfolioWorth.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux'
 import {numberWithCommas} from '../utils/validation'
 import holdingsData from '../mock/holdingsData'
 import store from '../stores/configureStore'
